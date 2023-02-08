71a370b2174e85c1b30220b800731b9abc91caf9
Smriti Vipin Madangarli <90612640+SmritiVM@users.noreply.github.com>
2023-02-08 16:36:21 +0530

    Working
    Left factoring, left recursion, first complete

---
+++ Predictive_parser.py
@@ -0,0 +1,265 @@
+#Left recursion, left factoring, firsts complete
+grammar = open("Grammar_.txt")
+
+V = set()
+T =  set()
+P_LHS = []
+P_RHS = []
+S = grammar.read(1)
+grammar.seek(0)
+
+for production in grammar:
+    lhs, rhs = production.split()
+
+    #adding the production
+    P_LHS.append(lhs)
+    P_RHS.append(rhs)
+
+    #adding the variables
+    V.add(lhs)
+
+    #adding terminals
+    for char in rhs:
+        T.add(char)
+T = T - V
+
+def print_productions(P_LHS, P_RHS):
+    for i in range(len(P_LHS)):
+        print(P_LHS[i] , '-->', P_RHS[i])
+        
+print("GRAMMAR")
+print("V: ", V)
+print("T: ", T)
+print_productions(P_LHS, P_RHS)
+print("S: ", S)
+
+
+
+#Removing left recursion
+new_state = 90
+def add_new_production(new_P_LHS, new_P_RHS, lhs, rhs):
+    global new_state
+    non_terminal = chr(new_state)
+    new_state -= 1
+    rule = rhs[1:] + non_terminal
+    
+    new_P_LHS.append(non_terminal)
+    new_P_RHS.append(rule)
+    new_P_LHS.append(non_terminal)
+    new_P_RHS.append('epsilon')
+
+    return non_terminal
+
+def modify_productions(P_LHS, P_RHS, lhs, new_P_LHS, new_P_RHS, non_terminal):
+    alt_pdtn_present = 0
+    for i in range(len(P_LHS)):
+        if P_LHS[i] == lhs and P_RHS[i][0] != lhs:
+            alt_pdtn_present = 1
+            new_P_LHS.append(lhs)
+            new_P_RHS.append(P_RHS[i] + non_terminal)
+
+    if not alt_pdtn_present:
+        new_P_LHS.append(lhs)
+        new_P_RHS.append(non_terminal)
+            
+def remove_left_recursion(P_LHS, P_RHS):
+    n = len(P_LHS)
+    new_P_LHS = []
+    new_P_RHS = []
+    left_recursive_nts = []
+    for i in range(n):
+        lhs = P_LHS[i]
+        rhs = P_RHS[i]
+        if rhs[0] == lhs:
+            left_recursive_nts.append(lhs)
+            new_non_terminal = add_new_production(new_P_LHS, new_P_RHS, lhs, rhs)
+            modify_productions(P_LHS, P_RHS, lhs, new_P_LHS, new_P_RHS, new_non_terminal)
+
+        if lhs not in left_recursive_nts:
+            new_P_LHS.append(P_LHS[i])
+            new_P_RHS.append(P_RHS[i])
+
+    return new_P_LHS, new_P_RHS
+
+P_LHS, P_RHS = remove_left_recursion(P_LHS, P_RHS)
+print("\nGrammar after removing left recursion: ")
+print_productions(P_LHS, P_RHS)
+
+#Modifying Variables list
+V = V.union(set(P_LHS))
+
+
+#Removing left factoring
+def longest_common_prefix(strs):
+    prefix = ""
+    if not strs:
+        return prefix
+            
+    for index in range(len(strs[0])):
+        for s in strs[1:]:
+            if index == len(s) or s[index] != strs[0][index]:
+                return prefix
+            
+        prefix += strs[0][index]
+        
+    return prefix
+
+def generate_pdtn_dictionary(P_LHS, P_RHS):
+    productions = {}
+    for i in range(len(P_LHS)):
+        lhs = P_LHS[i]
+        rhs = P_RHS[i]
+        if lhs not in productions:
+            productions[lhs] = [rhs]
+        else:
+            productions[lhs].append(rhs)
+    return productions
+
+
+def remove_left_factoring(P_LHS, P_RHS):
+    global new_state
+    productions = generate_pdtn_dictionary(P_LHS, P_RHS)
+    
+    for variable in productions.copy():
+        rules = productions[variable]
+        if len(rules) == 1:
+            continue
+        lcs = longest_common_prefix(rules)
+        length_lcs = len(lcs)
+        if length_lcs < 1:
+            continue
+        new_rules = []
+        for rule in rules:
+            rhs = rule[length_lcs:]
+            if len(rhs) == 0:
+                rhs = 'epsilon'
+            new_rules.append(rhs)
+
+        non_terminal = chr(new_state)
+        productions.update({variable : [lcs + non_terminal]})
+        productions[non_terminal] = new_rules
+
+        new_state -= 1
+    
+    return productions
+    
+    
+
+productions = remove_left_factoring(P_LHS, P_RHS)
+V = V.union(set(productions.keys()))
+
+def print_productions(productions):
+    for variable in productions:
+        for rule in productions[variable]:
+            print(variable, '-->', rule)
+
+print("\nGrammar after removing left factoring: ")
+print_productions(productions)
+
+
+#Computing first
+def find_first(variable, productions, first, wait):
+    if variable not in first:
+        first[variable] = []
+    for rule in productions[variable]:
+        if rule == 'epsilon':
+            next_symbol = 'epsilon'
+        else:
+            next_symbol = rule[0]
+        if next_symbol in T or next_symbol == 'epsilon':
+            first[variable].append(next_symbol)
+            
+
+        else:
+            if next_symbol in first:
+                first[variable].extend(first[next_symbol])
+
+            else:
+                wait.append(variable)
+    return wait
+
+def empty_waitlist(wait, productions, first):
+    while wait:
+        variable = wait.pop()
+        wait = find_first(variable, productions, first, wait)
+        
+
+def compute_firsts(productions):
+    wait = []
+    first = {}
+    for variable in productions:
+        find_first(variable, productions, first, wait)
+    empty_waitlist(wait, productions, first)
+
+    print("\nFirsts: ")
+    for variable in first:
+        print(variable, first[variable])
+
+    return first
+
+first = compute_firsts(productions)
+
+
+##Computing follow
+def get_key(val, my_dict):
+    for key, value in my_dict.items():
+        if val == value:
+            return key
+
+def find_follow_for_epsilon(variable, productions, rules, follow, wait):
+    parent = get_key(rules, productions)
+    if parent in follow:
+        follow[variable].union(follow[parent])
+    else:
+        wait.append(variable)
+    return wait
+        
+def find_follow(variable, productions, follow, wait, first):
+    if variable not in follow:
+        follow[variable] = set()
+    if variable == S:
+        follow[variable].add('$')
+
+    P_RHS = productions.values()
+    
+    for rules in P_RHS:
+        for rule in rules:
+            index = rule.find(variable)
+            if index == -1:
+                continue
+        
+            if index == len(rule) - 1:
+                wait = find_follow_for_epsilon(variable, productions, rules, follow, wait)
+
+            elif rule[index + 1] in T:
+                follow[variable].add(rule[index + 1])
+
+            elif rule[index + 1] in V:
+                next_symbol = rule[index + 1]
+                parent_first = first[next_symbol]
+                if 'epsilon' in parent_first:
+                    wait = find_follow_for_epsilon(variable, productions, rules, follow, wait)
+                for symbol in parent_first:
+                    if symbol != 'epsilon':
+                        follow[variable].add(symbol)
+                
+    
+    return wait
+
+def empty_waitlist(wait, productions, follow, first):
+    while wait:
+        variable = wait.pop()
+        print(variable, "wait")
+        wait = find_follow(variable, productions, follow, wait, first)
+
+def compute_follows(productions, first):
+    follow = {}
+    wait = []
+    for variable in productions:
+        find_follow(variable, productions, follow, wait, first)
+    empty_waitlist(wait, productions, follow, first)
+
+    print(follow)
+
+compute_follows(productions, first)
+            
